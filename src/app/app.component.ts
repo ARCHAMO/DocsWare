cfdf1d8ba1792f0ca5bcdedc3ce6e53d66903e96
Arnold Charris <acharris@consware.com.co>
2022-10-31 13:30:49 -0500

    Se agrega llamado por Endpoint para buscar la data formateada a mostrar.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { HttpBaseService } from './services/http-base.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'DocsWare';
+
+  public Editor = ClassicEditor;
+
+  public htmlData = '<p>Hello, world!</p>';
+
+  public config = {
+    toolbar: []
+  };
+
+  constructor(
+    private _httpService: HttpBaseService
+  ) {
+
+  }
+
+
+  mostrarData() {
+    console.log(this.htmlData);
+
+  }
+
+  setData() {
+    this._httpService.postMethod({}).subscribe(response => {
+      console.log(response);
+      this.htmlData = response.data;
+    });
+  }
 }
