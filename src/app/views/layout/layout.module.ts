95b550c4b110bb50b477fc21c7b481ecc75b605e
Arnold Dario Charris Molina <acharris@consware.com.co>
2022-11-05 07:58:49 -0500

    Estructuracion de la plantilla en el modulo de projects

--- src/app/views/layout/layout.module.ts
+++ src/app/views/layout/layout.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
 import { BaseComponent } from './base/base.component';
 import { RouterModule } from '@angular/router';
 
@@ -10,12 +9,15 @@ import { RouterModule } from '@angular/router';
   declarations: [
     FooterComponent,
     HeaderComponent,
-    SidebarComponent,
     BaseComponent,
   ],
   imports: [
     CommonModule,
     RouterModule
   ],
+  exports: [
+    HeaderComponent,
+    FooterComponent,
+  ]
 })
 export class LayoutModule { }
